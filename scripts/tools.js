fa53ead693c400a3c05f732ddc2cdf6049ffce6e
Herman Torjussen <herman.torjussen@gmail.com>
2018-03-09 17:13:17 +0100

    clean up tools, fix encoding bug

--- scripts/tools.js
+++ scripts/tools.js
@@ -1,29 +1,17 @@
 const compose = (...fs) => 
     x => fs.reduce((acc, f) => f(acc), x);
 
-const print = (...args) => {
+const print = (...args) =>
     console.log(...args)
-}
 
 const random = (min, max) =>
     Math.floor(Math.random() * (max - min + 1) + min)
 
-const randomColor = () =>
-    [0, 0, 0].map(c => random(0, 255)).concat(1.0)
-
-const rgbaString = (rgba) => {
-    return 'rgba(' +
-        rgba.slice(0, 3)
-        .map(Math.floor)
-        .concat(rgba[3])
-        .join() + ')'
-}
-
 const toBase64 = (str) => 
-    btoa(unescape(encodeURIComponent(str)))
+    btoa(encodeURIComponent(str))
 
 const fromBase64 = (str) => 
-    decodeURIComponent(escape(atob(str)))
+    decodeURIComponent(atob(str))
 
 const setQueryParam = (name, value) => {
     let newurl = window.location.protocol + '//' + 
@@ -35,8 +23,6 @@ export {
     compose, 
     print, 
     random, 
-    randomColor, 
-    rgbaString,
     toBase64,
     fromBase64,
     setQueryParam
