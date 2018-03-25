c6e023140d2caeff6c24901536f9605097c9369c
Herman Torjussen <herman.torjussen@gmail.com>
2018-03-25 19:38:33 +0200

    more useful functions

---
+++ examples/ellipse.js
@@ -0,0 +1,4 @@
+// dashed, rotated ellipse
+ldash([1,2,9])
+lwidth(2)
+sellips(w/2,h/2,50,240,45)
--- scripts/index.js
+++ scripts/index.js
@@ -12,8 +12,10 @@ window.frameReset = true
 window.begin = () => graphics.beginPath()
 window.close = () => graphics.closePath()
 window.stroke = () => graphics.stroke()
+window.fill = () => graphics.fill()
 window.line = (...args) => graphics.lineTo(...args)
 window.move = (...args) => graphics.moveTo(...args)
+window.rect = (...args) => graphics.rect(...args)
 window.srect = (...args) => graphics.strokeRect(...args)
 window.frect = (...args) => graphics.fillRect(...args)
 window.font = (arg) => graphics.font = arg
@@ -32,7 +34,7 @@ window.tslate = (...args) => graphics.translate(...args)
 window.scale = (...args) => graphics.scale(...args)
 window.treset = () => graphics.setTransform(1, 0, 0, 1, 0, 0)
 window.second = () => new Date().getSeconds()
-window.millis = () => (new Date()) - startDate
+window.millis = window.ms = () => (new Date()) - startDate
 window.fps = (arg) => window.frameRate = arg
 window.freset = (arg) => window.frameReset = arg
 window.rand = random
@@ -43,6 +45,28 @@ window.min = Math.min
 window.max = Math.max
 window.map = (e,t,n,r,i) => r+(i-r)*((e-t)/(n-t))
 window.rgba = (...colors) => `rgba(${[...colors].map(c => c.toFixed(0))})`
+window.comp = (arg) => graphics.globalCompositeOperation = arg
+window.doffset = (arg) => graphics.lineDashOffset = arg
+window.ldash = (arg) => graphics.setLineDash(arg)
+window.ljoin = (arg) => graphics.lineJoin = arg
+window.shblevel = (arg) => graphics.shadowBlur = arg
+window.shcolor = (arg) => graphics.shadowColor = arg
+window.shoffx = (arg) => graphics.shadowOffsetX = arg
+window.shoffy = (arg) => graphics.shadowOffsetY = arg
+window.ellips = (x, y, w, h, r) => 
+    graphics.ellipse(x, y, w, h, r ? r * Math.PI / 180 : 0, 0 , 2 * Math.PI)
+window.sellips = (...args) => { begin(); ellips(...args); stroke() }
+window.fellips = (...args) => { begin(); ellips(...args); fill() }
+window.lgrad = (...args) => {
+    let g = graphics.createLinearGradient(...args)
+    g.stop = (...args) => { g.addColorStop(...args); return g }
+    return g
+}
+window.rgrad = (...args) => {
+    let g = graphics.createRadialGradient(...args)
+    g.stop = (...args) => { g.addColorStop(...args); return g }
+    return g
+}
 
 window.onclick = editor.focus
 window.onresize = graphics.resize
