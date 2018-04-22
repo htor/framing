567e45fe464a231f41b9488c753249109bce76f3
Herman Torjussen <herman.torjussen@gmail.com>
2018-04-22 22:01:56 +0200

    fix example error

--- examples/colors-rgba.js
+++ examples/colors-rgba.js
@@ -1,9 +1,8 @@
 // color pattern using rgba function
-let x,y,c,s=8
+let x,y,c,s=11
 for (x=0;x<w;x+=s) {
   for (y=0;y<h;y+=s) {
-    c=[255&(max(x,y)),255&(x^y),255&~(x*y),1]
-    fstyle(rgba(c))
+    fstyle(rgba(255&(max(x,y)),+255&(x^y),+255&~(x*y),1))
     frect(x,y,s,s)
   }
 }
